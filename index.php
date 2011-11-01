a0cd81b8d0f03471a4d1bf13a04f5f3b2265a211
Daniel Scott <ds191@hw.ac.uk>
2011-11-01 14:58:57 +0000

    Updated image slider.

--- index.php
+++ index.php
@@ -4,8 +4,12 @@ include_once('smarty/Smarty.class.php');
 
 $smarty = new Smarty();
 
-$gScripts = array();
+$gScripts = array('scripts/jquery.slideViewerPro.1.5.js', 'scripts/jquery.timers-1.2.js', 'http://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js');
+
+$gStyles = array('style/svwp_style.css');
 
 $smarty->assign("scripts",$gScripts);
+
+$smarty->assign("styles",$gStyles);
 
 $smarty->display("base.tpl");
