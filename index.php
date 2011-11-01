c152f9841eabc8b80a2dcc2635f0a5a2d5aae657
Daniel Scott <ds191@hw.ac.uk>
2011-11-01 15:31:10 +0000

    still working on the image slider.

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ include_once('smarty/Smarty.class.php');
 
 $smarty = new Smarty();
 
-$gScripts = array('scripts/jquery.slideViewerPro.1.5.js', 'scripts/jquery.timers-1.2.js', 'http://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js');
+$gScripts = array('scripts/jquery.slideViewerPro.1.5.js', 'scripts/jquery.timers-1.2.js', 'http://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js', 'scripts/imageslider.js');
 
 $gStyles = array('style/svwp_style.css');
 
---
+++ scripts/imageslider.js
@@ -0,0 +1,15 @@
+$(window).bind("load", function() {
+	$("div#my-folio-of-works").slideViewerPro({
+		thumbs: 6, 
+		autoslide: true, 
+		asTimer: 3500, 
+		typo: true,
+		galBorderWidth: 0,
+		thumbsBorderOpacity: 0, 
+		buttonsTextColor: "#707070",
+		buttonsWidth: 40,
+		thumbsActiveBorderOpacity: 0.8,
+		thumbsActiveBorderColor: "aqua",
+		shuffle: true
+	});
+});
