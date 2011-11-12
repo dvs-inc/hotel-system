3ee2c3634aa8ced0bb790ddc556bdbe9b491b2fa
Simon Walker <github@stwalkerster.co.uk>
2011-11-12 17:12:25 +0000

    class for messaging system

---
+++ include/Message.php
@@ -0,0 +1,16 @@
+<?php
+// check for invalid entry point
+if(!defined("HMS")) die("Invalid entry point");
+
+class Message extends DataObject
+{
+	public static function getById(int $id)
+	{
+	
+	}
+	
+	public function save()
+	{
+	
+	}
+}
