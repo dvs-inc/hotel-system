2a85546a7e61d52df22073f40f48aba12db83747
Simon Walker <github@helpmebot.org.uk>
2012-01-18 18:47:31 +0000

    tweak to show dirty status on version information

--- include/ManagementPage/MPageGit.php
+++ include/ManagementPage/MPageGit.php
@@ -7,6 +7,6 @@ class MPageGit extends ManagementPageBase
 	protected function runPage()
 	{
 		$this->mBasePage="mgmt/git.tpl";
-		$this->mSmarty->assign("softwareVersion", exec("git describe --always"));
+		$this->mSmarty->assign("softwareVersion", exec("git describe --always --dirty"));
 	}
 }
