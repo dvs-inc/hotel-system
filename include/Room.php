88a9c66e3b111c103000017c75c42fe1fb31591a
Simon Walker <github@stwalkerster.co.uk>
2012-01-24 11:44:07 +0000

    room class start - needs clarification with database structure

---
+++ include/Room.php
@@ -0,0 +1,32 @@
+<?php
+// check for invalid entry point
+if(!defined("HMS")) die("Invalid entry point");
+
+class Room extends DataObject
+{
+	protected $type;
+	protected $maxPeople;
+	protected $minPeople;
+	protected $price;
+
+	public static function getById($id)
+	{
+		global $gDatabase;
+		$statement = $gDatabase->prepare("SELECT * FROM room WHERE id = :id LIMIT 1;");
+		$statement->bindParam(":id", $id);
+		
+		$statement->execute();
+		
+		$resultObject = $statement->fetchObject("Room");
+		if($resultObject != false)
+		{
+			$resultObject->isNew = false;
+		}
+		
+		return $resultObject;
+	}
+	
+	public abstract function save();
+
+	public abstract function delete();
+}
