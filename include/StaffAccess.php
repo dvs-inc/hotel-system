ea6fc5e63c4a643c442170b35632e1ca8860ad82
Simon Walker <github@stwalkerster.co.uk>
2012-02-14 02:42:57 +0000

    #17: staff access data object

---
+++ include/StaffAccess.php
@@ -0,0 +1,69 @@
+<?php
+// check for invalid entry point
+if(!defined("HMS")) die("Invalid entry point");
+
+class StaffAccess extends DataObject
+{
+	protected $action;
+	protected $level;
+
+	public static function getById($id)
+	{
+		global $gDatabase;
+		$statement = $gDatabase->prepare("SELECT * FROM staffAccess WHERE id = :id LIMIT 1;");
+		$statement->bindParam(":id", $id);
+
+		$statement->execute();
+
+		$resultObject = $statement->fetchObject("StaffAccess");
+		if($resultObject != false)
+		{
+			$resultObject->isNew = false;
+		}
+		return $resultObject;
+	}
+
+	public function save()
+	{
+		global $gDatabase;
+
+		if($this->isNew)
+		{ // insert
+			$statement = $gDatabase->prepare("INSERT INTO staffAccess VALUES (null, :action, :accesslvl);");
+			$statement->bindParam(":action", $this->action);
+			$statement->bindParam(":accesslvl", $this->level);
+			if($statement->execute())
+			{
+				$this->isNew = false;
+				$this->id = $gDatabase->lastInsertId();
+			}
+			else
+			{
+				throw new SaveFailedException();
+			}
+		}
+		else
+		{ // update
+			$statement = $gDatabase->prepare("UPDATE staffAccess SET action = :action, level = :accesslvl WHERE id = :id LIMIT 1;");
+			$statement->bindParam(":action", $this->action);
+			$statement->bindParam(":accesslvl", $this->level);
+			$statement->bindParam(":id", $this->id);
+
+			if(!$statement->execute())
+			{
+				throw new SaveFailedException();
+			}
+		}
+	}
+
+	public function delete()
+	{
+		global $gDatabase;
+		$statement = $gDatabase->prepare("DELETE FROM staffAccess WHERE id = :id LIMIT 1;");
+		$statement->bindParam(":id", $this->id);
+		$statement->execute();
+
+		$this->id=0;
+		$this->isNew = true;
+	}
+}
