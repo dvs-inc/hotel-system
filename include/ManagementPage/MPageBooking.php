b67dab9eb84c776871f67a445dc1005e28516e36
LauraMcCormack <laura.mccormack227@gmail.com>
2012-03-21 16:51:33 +0000

    added Booking page to management

---
+++ include/ManagementPage/MPageBooking.php
@@ -0,0 +1,235 @@
+<?php
+// check for invalid entry point
+if(!defined("HMS")) die("Invalid entry point");
+
+class MPageBooking extends ManagementPageBase
+{
+	public function __construct()
+	{
+		$this->mAccessName = "view-bookings";
+	}
+
+	protected function runPage()
+	{
+		$this->mSubMenu = array(
+			"MPageBookingList" => array(
+				"title" => "mpage-booking-list",
+				"link" => "/Booking?action=list",
+				),
+			"MPageBookingCreate" => array(
+				"title" => "mpage-booking-create",
+				"link" => "/Booking?action=create",
+				),
+
+		);
+	
+		$action = WebRequest::get("action");
+		switch($action)
+		{
+			case "del":
+				self::checkAccess("delete-booking");
+				//$this->doDeleteRoomAction();
+				break;
+			case "edit":
+				self::checkAccess("edit-booking");
+				//$this->showEditRoomPage();
+				break;
+			case "create":
+				self::checkAccess("create-booking");
+				//$this->showCreateRoomPage();
+				break;
+			case "list":
+			default:
+				$this->showListBookingPage();
+				break;
+		}
+	}
+/*
+	private function showCreateRoomPage()
+	{
+		if(WebRequest::wasPosted())
+		{
+			try{
+				// get variables
+				$rname = WebRequest::post("rname");
+				$rtype = WebRequest::postInt("rtype");
+				$rmin = WebRequest::postInt("rmin");
+				$rmax = WebRequest::postInt("rmax");
+				$rprice = WebRequest::postFloat("rprice");
+			
+				// data validation
+				if($rname == "")
+				{
+					throw new CreateRoomException("blank-roomname");
+				}	
+				
+				if($rtype == 0)
+				{
+					throw new CreateRoomException("blank-roomtype");
+				}
+				
+				if($rmax < 1 || $rmin < 0)
+				{
+					throw new CreateRoomException("room-capacity-too-small");
+				}
+				
+				if($rmin > $rmax)
+				{
+					throw new CreateRoomException("room-capacity-min-gt-max");
+				}
+				
+				if($rprice != abs($rprice))
+				{
+					throw new CreateRoomException("room-price-negative");
+				}
+				
+				
+				$room = new Room();
+				
+				// set values
+				$room->setName($rname);
+				$room->setType($rtype);
+				$room->setMinPeople($rmin);
+				$room->setMaxPeople($rmax);
+				$room->setPrice($rprice);
+				
+				
+				$room->save();
+
+				global $cScriptPath;
+				$this->mHeaders[] = "Location: {$cScriptPath}/Rooms";
+			}
+			catch (CreateRoomException $ex)
+			{
+				$this->mBasePage="mgmt/roomCreate.tpl";
+				$this->error($ex->getMessage());
+			}
+		}
+		else
+		{
+			$this->mBasePage="mgmt/roomCreate.tpl";
+		}
+		
+		$this->mSmarty->assign("rtlist", RoomType::$data);
+	}	
+	
+	private function showEditRoomPage()
+	{
+		if(WebRequest::wasPosted())
+		{
+			try{
+				// get variables
+				$rname = WebRequest::post("rname");
+				$rtype = WebRequest::postInt("rtype");
+				$rmin = WebRequest::postInt("rmin");
+				$rmax = WebRequest::postInt("rmax");
+				$rprice = WebRequest::postFloat("rprice");
+				$id = WebRequest::getInt("id");
+				
+				// data validation
+				if($rname == "")
+				{
+					throw new CreateRoomException("blank-roomname");
+				}	
+				
+				if($rtype == 0)
+				{
+					throw new CreateRoomException("blank-roomtype");
+				}
+				
+				if($rmax < 1 || $rmin < 0)
+				{
+					throw new CreateRoomException("room-capacity-too-small");
+				}
+				
+				if($rmin > $rmax)
+				{
+					throw new CreateRoomException("room-capacity-min-gt-max");
+				}
+				
+				if($rprice != abs($rprice))
+				{
+					throw new CreateRoomException("room-price-negative");
+				}
+				
+				
+				$room = Room::getById($id);
+				
+				if($room == null)
+				{
+					throw new Exception("Room does not exist");
+				}
+				
+				// set values
+				$room->setName($rname);
+				$room->setType($rtype);
+				$room->setMinPeople($rmin);
+				$room->setMaxPeople($rmax);
+				$room->setPrice($rprice);
+				
+				
+				$room->save();
+
+				global $cScriptPath;
+				$this->mHeaders[] = "Location: {$cScriptPath}/Rooms";
+			}
+			catch (CreateRoomException $ex)
+			{
+				$this->mBasePage="mgmt/roomEdit.tpl";
+				$this->error($ex->getMessage());
+			}
+		}
+		else
+		{
+			$this->mBasePage="mgmt/roomEdit.tpl";
+			
+			$room = Room::getById(WebRequest::getInt("id")) ;
+			
+			if($room == null)
+			{
+				throw new Exception("Room does not exist");
+			}
+			
+			$this->mSmarty->assign("roomid", $room->getId());
+			$this->mSmarty->assign("rname", $room->getName());
+			$this->mSmarty->assign("rmin", $room->getMinPeople());
+			$this->mSmarty->assign("rmax", $room->getMaxPeople());
+			$this->mSmarty->assign("rprice", $room->getPrice());
+			$this->mSmarty->assign("rtype", $room->getType()->getId());
+		}
+		
+		$this->mSmarty->assign("rtlist", RoomType::$data);
+	}	
+*/
+	private function showListBookingPage()
+	{
+		$idList = Booking::getIdList();
+
+		$custList = array();
+
+		foreach($idList as $id)
+		{
+			$custList[] = Booking::getById($id);
+		}
+
+		$this->mSmarty->assign("bookingList", $bookingList);
+
+		$this->mBasePage="mgmt/bookingList.tpl";
+	}
+/*
+	private function doDeleteRoomAction()
+	{
+		$rid=WebRequest::getInt("id");
+		if($rid < 1)
+				throw new Exception("RoomId too small");
+
+		if(Room::getById($rid) == null)
+				throw new Exception("Room does not exist");
+
+		Room::getById($rid)->delete();
+
+		global $cScriptPath;
+		$this->mHeaders[] = "Location: {$cScriptPath}/Rooms";
+	}
+*/
+}
