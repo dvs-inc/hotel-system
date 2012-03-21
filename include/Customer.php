21a332643968eb7beef75cebe28b4d4ed361053f
JamesAttree <ja158@hw.ac.uk>
2012-03-21 13:54:27 +0000

    Line 87:92 - Why are these different to the SQL Query?

--- include/Customer.php
+++ include/Customer.php
@@ -83,7 +83,7 @@ class Customer extends DataObject
 		global $gDatabase;
 		
 		if($this->isNew)
-		{ // insert
+		{ // insert (Why are these values different to the SQL Query?!)
 			$statement = $gDatabase->prepare("INSERT INTO customer VALUES (null, :firstname, :surname, :address, :email, :language");
 			$statement->bindParam(":adults", $this->firstname );
 			$statement->bindParam(":children",$this->surname );
