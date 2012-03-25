825ef3ab7e5e9d31d4f94fedb8adfaa8049a36c0
LauraMcCormack <laura.mccormack227@gmail.com>
2012-03-25 23:01:17 +0100

    fix

--- include/ManagementPage/MPageBookings.php
+++ include/ManagementPage/MPageBookings.php
@@ -149,7 +149,7 @@ class MPageBookings extends ManagementPageBase
 				
 				if($booking == null)
 				{
-					throw new Exception("Booking does not exist");
+					throw new CreateBookingException("Booking does not exist");
 				}
 				
 				// set values
@@ -182,15 +182,14 @@ class MPageBookings extends ManagementPageBase
 			{
 				throw new Exception("Booking does not exist");
 			}
-
+			
 			$this->mSmarty->assign("bookingid", $booking->getId());
-			$this->mSmarty->assign("bcust", $booking->getCustomerID());
+			$this->mSmarty->assign("bcust", $booking->getCustomer());
 			$this->mSmarty->assign("badults", $booking->getAdults());
 			$this->mSmarty->assign("bchildren", $booking->getChildren());
 			$this->mSmarty->assign("bstart", $booking->getStartDate());
 			$this->mSmarty->assign("bend", $booking->getEndDate());
 			$this->mSmarty->assign("bpromo", $booking->getPromocode());
-			
 		}
 		
 	}	
