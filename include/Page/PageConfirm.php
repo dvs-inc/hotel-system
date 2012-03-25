162b706d800fd20a7f30148d0fef997fcfd18410
Simon Walker <github@stwalkerster.co.uk>
2012-03-25 20:58:47 +0100

    ......

--- include/Page/PageConfirm.php
+++ include/Page/PageConfirm.php
@@ -6,33 +6,32 @@ class PageConfirm extends PageBase
 {
 	protected function runPage()
 	{
-			try{
-				$id = WebRequest::getInt("id");
-				$hash = WebRequest::get("hash");
-			
-				// data validation
+		try{
+			$id = WebRequest::getInt("id");
+			$hash = WebRequest::get("hash");
+		
+			// data validation
 
-				$customer = Customer::getById();
-				
-				if($customer==null)
-				{
-					throw new NonexistantObjectException();
-				}
+			$customer = Customer::getById();
 			
-				$customer->confirmEmail($hash);
-			
-				// save
-				$customer->save();
-				
-				Session::setLoggedInCustomer($id);
-				
-				$this->mSmarty->assign("content", Message::getMessage("mail-confirmed"));
-			}
-			catch (NonexistantObjectException $ex)
+			if($customer==null)
 			{
-				global $cScriptPath;
-				$this->mHeaders[] = "Location: {$cScriptPath}";
+				throw new NonexistantObjectException();
 			}
+		
+			$customer->confirmEmail($hash);
+		
+			// save
+			$customer->save();
+			
+			Session::setLoggedInCustomer($id);
+			
+			$this->mSmarty->assign("content", Message::getMessage("mail-confirmed"));
+		}
+		catch (NonexistantObjectException $ex)
+		{
+			global $cScriptPath;
+			$this->mHeaders[] = "Location: {$cScriptPath}";
 		}
 	}
 }
