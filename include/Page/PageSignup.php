cf248f238a801d4223f003c164b7e1cc2048cf76
JamesAttree <ja158@hw.ac.uk>
2012-03-25 14:01:58 +0200

    Update include/Page/PageSignup.php

--- include/Page/PageSignup.php
+++ include/Page/PageSignup.php
@@ -6,6 +6,84 @@ class PageSignup extends PageBase
 {
 	protected function runPage()
 	{
+		$this->mBasePage = "signup.tpl";
 		
+		if(Session::isCustomerLoggedIn == true){
+			// redirect to main page
+			$this->mHeaders[] = "HTTP/1.1 303 See Other";
+			$this->mHeaders[] = "Location: " . $cWebPath . "/index.php";
+		}
+			
+		$action = WebRequest::get("action");
+		switch($action)
+		{
+			case "signup":
+			default:
+				$this->signupCustomer();
+				break;
+		}
+	}
+	
+	private function signupCustomer()
+	{
+		try{
+			$suTitle = WebRequest::post("suTitle");
+			$suFirstname = WebRequest::post("suFirstname");
+			$suLastname = WebRequest::post("suLastname");
+			$suAddress = WebRequest::post("suAddress");
+			$suCity = WebRequest::post("suCity");
+			$suPostcost = WebRequest::post("suPostcode");
+			$suCountry = WebRequest::post("suCountry");
+			$suEmail = WebRequest::post("suEmail");
+			$suPassword = WebRequest::post("suPassword");
+			$suConfirm = WebRequest::post("suConfirm");
+		
+			// data validation
+			if($suTitle == ""){throw new CreateCustomerException("Title not specified");}
+			if($suFirstname == ""){throw new CreateCustomerException("Firstname not specified");}
+			if($suLastname == ""){throw new CreateCustomerException("Lastname not specified");}
+			if($suAddress == ""){throw new CreateCustomerException("Address not specified");}
+			if($suCity == ""){throw new CreateCustomerException("City not specified");}
+			if($suPostcode == ""){throw new CreateCustomerException("Postcode not specified");}
+			if($suCountry == ""){throw new CreateCustomerException("Country not specified");}
+			if($suEmail == ""){throw new CreateCustomerException("Email not specified");}
+			if($suPassword == ""){throw new CreateCustomerException("Password not specified");}
+			if($suConfirm == ""){throw new CreateCustomerException("Confirmed password not specified");}
+			if($suPassword != $suConfirm){throw new CreateCustomerException("Password mismatch");}
+
+			$customer = new Customer();
+			
+			if($suPassword != "" && $suPassword == $suConfirm)
+			{
+				$customer->setPassword($suPassword);
+			}
+		
+			// set values
+			$customer->setTitle($suTitle);
+			$customer->setFirstname($suFirstName);
+			$customer->setSurname($suLastname);
+		
+			$address = new Address();
+			$address->setLine1($suAddress);
+			$address->setCity($suCity);
+			$address->setPostCode($suPostcode);
+			$address->setCountry($suCountry);
+			$address->save();
+		
+			$customer->setAddress($address);
+		
+			$customer->setEmail($suEmail);
+		
+			// save it
+			$customer->save();
+			
+			global $cScriptPath;
+			this->mHeaders[] = "Location: {cScriptPath}/Signup";
+		}
+		catch (CreateCustomerException $ex)
+		{
+			$this->mBasePage="signup.tpl";
+			$this->error($ex->getMessage());
+		}
 	}
 }
