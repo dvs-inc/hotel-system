5d1640f538611a331469bbe1d7dea49abdc8246a
James Attree <ja158@hw.ac.uk>
2012-03-26 18:55:28 +0100

    added changePassword files

---
+++ include/Page/PageChangePassword.php
@@ -0,0 +1,62 @@
+<?php
+// check for invalid entry point
+if(!defined("HMS")) die("Invalid entry point");
+
+class PageForgotPassword extends PageBase
+{
+protected function runPage()
+{
+
+try{
+
+	$email = WebRequest::get("email");
+	$hash = WebRequest::get("hash");
+
+	$customer = Customer::getByEmail($email);
+
+	if($customer==null)
+	{
+		throw new NonExistantObjectException();
+		
+	}
+
+	if(WebRequest::wasPosted() && $customer!=null)
+	{
+		try{
+			$suPassword = WebRequest::post("suPassword");
+			$suConfirm = WebRequest::post("suConfirm");
+
+			// validation
+			
+			if($suPassword == ""){throw new CreateCustomerException("Password not specified");}
+			if($suConfirm == ""){throw new CreateCustomerException("Confirmed password not specified");}
+			if($suPassword != $suConfirm){throw new CreateCustomerException("Password mismatch");}
+	
+			// validation			
+
+			if ($suPassword != "" && $suPassword == $suConfirm)
+			{
+				$customer->setPassword($suPassword);
+			}	
+
+			$customer->save();
+		}
+
+		catch (CreateCustomerException $ex)
+		{	
+			$this->mBasePage="changePassword.tpl";
+			$this->error($ex->getMessage());
+		}
+
+	else
+	{
+		$this->mBasePage="changePassword.tpl";
+	}
+
+catch (NonexistantObjectException $ex)
+{
+	global $cScriptPath;
+	$this->mHeaders[] = "Location: {$cScriptPath}";
+}
+}
+}
