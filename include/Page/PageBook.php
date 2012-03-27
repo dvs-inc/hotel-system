8ffd3f34b7b7153e4935d2c3cd1ff1fe16d53e24
Simon Walker <github@stwalkerster.co.uk>
2012-03-27 15:53:29 +0100

    pull in user data from customer object

--- include/Page/PageBook.php
+++ include/Page/PageBook.php
@@ -40,5 +40,31 @@ class PageBook extends PageBase
 			$this->mSmarty->assign("valQbChildren", "");
 			$this->mSmarty->assign("valQbPromoCode", "");
 		}
+		
+		if(Session::isCustomerLoggedIn())
+		{
+			$customer = Customer::getById(Session::getLoggedInCustomer());
+		
+			$this->mSmarty->assign("qbTitle",$customer->getTitle());
+			$this->mSmarty->assign("qbFirstname",$customer->getFirstname());
+			$this->mSmarty->assign("qbLastname",$customer->getSurname());
+			$this->mSmarty->assign("qbAddress",$customer->getAddress()->getLine1());
+			$this->mSmarty->assign("qbCity",$customer->getAddress()->getCity());
+			$this->mSmarty->assign("qbPostcode",$customer->getAddress()->getPostcode());
+			$this->mSmarty->assign("qbCountry",$customer->getAddress()->getCountry());
+			$this->mSmarty->assign("qbEmail",$customer->getEmail());
+			
+		}
+		else
+		{
+			$this->mSmarty->assign("qbTitle","");
+			$this->mSmarty->assign("qbFirstname","");
+			$this->mSmarty->assign("qbLastname","");
+			$this->mSmarty->assign("qbAddress","");
+			$this->mSmarty->assign("qbCity","");
+			$this->mSmarty->assign("qbPostcode","");
+			$this->mSmarty->assign("qbEmail","");
+			$this->mSmarty->assign("qbCountry"," ");
+		}
 	}
 }
