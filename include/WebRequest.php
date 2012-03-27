ec938862116e672d6600ec0b5911cd2051029f33
Simon Walker <github@stwalkerster.co.uk>
2012-03-27 05:13:57 +0100

    fffffffffffffffffffffffffffffs

--- include/WebRequest.php
+++ include/WebRequest.php
@@ -152,7 +152,7 @@ class WebRequest
 		$data = str_replace("-->", "", $data);
 		
 		// show that rogue data as a comment after everything
-		print "<!--" . $data . "-->"
+		print "<!--" . $data . "-->";
 		
 		// flush the buffer to the browser
 		ob_flush();
